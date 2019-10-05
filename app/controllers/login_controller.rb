c5456585f349fe8b8e13db545109e234284da0cf
felunka <felixvontiedemann@gmail.com>
2019-10-05 11:09:02 +0000

    Add API logic to login_controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,30 @@
 class LoginController < ApplicationController
+  require 'net/http'
+  require 'json'
+  
   def show
     
   end
   
   def commit
-    byebug
+    uri = URI.parse('https://geco.ethz.ch/api/v2/auth')
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({'email': params[:username], 'password': params[:password]})
+    request['Content-Type'] = 'application/x-www-form-urlencoded'
+    request['X-API-KEY'] = Rails.configuration.api_key
+    
+    response = http.request(request)
+    
+    if response.code == '200'
+      render "success"
+    else
+      message  = 'Username or password incorrect.'
+      flash[:danger] = message
+      render "show"
+    end
   end
 end
