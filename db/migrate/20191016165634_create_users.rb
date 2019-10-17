13246a8bd493058cf222bbeb993ada09eca4da40
felunka <felixvontiedemann@gmail.com>
2019-10-17 22:17:53 +0000

    Remove ID from user

--- db/migrate/20191016165634_create_users.rb
+++ db/migrate/20191016165634_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    create_table :users, :id => false do |t|
       t.string :mac
       t.string :ip
       t.string :switch
