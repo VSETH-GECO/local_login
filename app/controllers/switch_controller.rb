d565a0a424cef370cdac21598682de121a4d50dd
felunka <felixvontiedemann@gmail.com>
2019-10-17 22:59:52 +0000

    Add success page for switch success

--- app/controllers/switch_controller.rb
+++ app/controllers/switch_controller.rb
@@ -5,6 +5,7 @@ class SwitchController < ApplicationController
 
   def commit
     @user = User.find_by(ip: request.remote_ip)
+    @vlan = params[:vlan]
     BouncerJob.new(clientMAC: @user.mac, targetVLAN: params[:vlan]).save!
   end
 end
