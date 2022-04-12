89b95513466ccc0a364e562764f6a3935a0e2f72
Maximilian Falkenstein <maxf@njsm.de>
2022-04-12 18:48:45 +0200

    Try to fix VLAN assignment

--- app/controllers/switch_controller.rb
+++ app/controllers/switch_controller.rb
@@ -6,6 +6,26 @@ class SwitchController < ApplicationController
   def commit
     @user = User.find_by(ip: request.remote_ip)
     @vlan = params[:vlan]
+
+    if @user.nil?
+      message  = 'Host not found in RADIUS database.'
+      flash[:danger] = message
+      render "show"
+      return
+    end
+
+    if @vlan.nil?
+      message  = 'VLAN not specified.'
+      flash[:danger] = message
+      render "show"
+    end
+
+    if @vlan < 490 || @vlan > 495
+      message  = 'VLAN invalid'
+      flash[:danger] = message
+      render "show"
+    end
+
     BouncerJob.new(clientMAC: @user.mac, targetVLAN: params[:vlan]).save!
   end
 end
